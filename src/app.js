ea940bb81214f5911c82d139b028764a348092e7
londaka <sanjayapokhrel41@gmail.com>
2024-12-14 19:58:54 +0545

    setup configured

--- src/app.js
+++ src/app.js
@@ -17,4 +17,16 @@ app.use(cookieParser())
 
 //export default app
 //or
+
+
+
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+//http://localhost:8000/api/v1/users/register
+
 export { app }
--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,43 +1,9 @@
 const asyncHandler = (requestHandler) => {
- (req, res, next) =>{
-    Promise.resolve(requestHandler(req, res, next))
-    .catch((err) => next(err))
- }
-}
-
-
-
-export {asyncHandler}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const asyncHandler = (fn) => async (req, res, next) => {
-    try{
-        await fn(req, res, next)
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        })
-    }
-}
-    */
-
+    return (req, res, next) => {
+      Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err));
+    };
+  };
+  
+  export { asyncHandler };
+  
